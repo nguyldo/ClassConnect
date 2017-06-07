73847785e43100275758ffb0e9ea6afb53c9d0aa
nguyldo <nguyldo@gmail.com>
2017-06-07 00:37:38 -0700

    implemented firebase

--- ClassConnect/LoginViewController.swift
+++ ClassConnect/LoginViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var idTextField: UITextField!
+    private var smallId: String!
+    
+    var ref: FIRDatabaseReference = FIRDatabase.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +27,44 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func signIn(_ sender: UIButton) {
+        /*
+        if let localId = idTextField.text {
+            ref.child("allCodes").observeSingleEvent(of: .value, with: { (snapshot) in
+                let value = snapshot.value as! NSDictionary
+                if let _ = value[localId] {
+                    print(localId)
+                    self.smallId = localId
+                }
+            }, withCancel: { (error) in
+                print(error.localizedDescription)
+            })
+        }*/
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "loginSegue" {
+            /*
+            let preVc = segue.destination as! UINavigationController
+            let vc = preVc.topViewController as! QuestionsViewController
+            if let localId = smallId {
+                UserDefaults.standard.setValue(localId, forKey: "idDefault")
+            }
+ */
+            if let localId = idTextField.text {
+                ref.child("allCodes").observeSingleEvent(of: .value, with: { (snapshot) in
+                    let value = snapshot.value as! NSDictionary
+                    if let _ = value[localId] {
+                        print(localId)
+                        
+                        UserDefaults.standard.set(localId, forKey: "idDefault")
+                    }
+                }, withCancel: { (error) in
+                    print(error.localizedDescription)
+                })
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
